412351bccfbf7cd4a62027dfa3fc058a1e4505e0
idanceinbetween <46828701+idanceinbetween@users.noreply.github.com>
2019-05-14 17:50:55 +0100

    Done.

--- src/components/ControlledInput.js
+++ src/components/ControlledInput.js
@@ -1,1 +1,10 @@
-// Code ControlledInput Component Here
+import React, { Component } from "react";
+
+class ControlledInput extends Component {
+  state = {};
+  render() {
+    return "yo";
+  }
+}
+
+export default ControlledInput;
